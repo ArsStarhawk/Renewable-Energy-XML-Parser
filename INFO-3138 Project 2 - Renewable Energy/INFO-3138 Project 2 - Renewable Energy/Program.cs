4be1ceabebb38df5c02d34e48c8d5a77441d1a7a
James Kidd <garlicraspberry@hotmail.com>
2020-07-06 23:56:22 -0400

    Initial Start

---
+++ INFO-3138 Project 2 - Renewable Energy/INFO-3138 Project 2 - Renewable Energy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INFO_3138_Project_2___Renewable_Energy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            
+
+
+        }
+    }
+}
